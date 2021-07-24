191abf5d60b324cf5e0445cc4a75921ce5976696
ThirashaPraween <thirashawijerathna@gmail.com>
2021-07-24 17:47:10 +0530

    new web stable 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from './pages/NotFound'
 import Projects from './pages/Projects'
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects" component={Projects} />
