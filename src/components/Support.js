db125a99a5cc270c18ec3b3e9781a7d6fff0046a
ThirashaPraween <thirashawijerathna@gmail.com>
2021-07-24 22:31:38 +0530

    new web stable 9

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import "../pages/Home.css";
 import "./Support.css";
 
 export default function Support({support}) {
+
+  const [hoverState, setHoverState] = useState(false)
+
   return (
     <div
       className="content-box-support"
-      style={{ cursor: "pointer" }}
+      style={{ cursor: "pointer", boxShadow: hoverState ? "0 3px 70px " + support.color : "0 3px 70px #FFDD00", border: hoverState ? "2px solid " + support.color : "2px solid #FFDD00" }}
       onClick={() => window.open(support.redirect, '_blank')}
+      onMouseEnter={() => setHoverState(true)}
+      onMouseLeave={() => setHoverState(false)}
     >
       <div className="support">
         <img
