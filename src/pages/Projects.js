e53efa6d5c5b887a08d88e908b45f3d9f40dab41
ThirashaPraween <thirashawijerathna@gmail.com>
2021-07-25 12:26:18 +0530

    new web stable 11

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import "./Home.css";
 import "./Projects.css";
 import Project from "../components/Project";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Projects() {
   const date = new Date().getFullYear();
 
   const [projects, setProjects] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchedArray, setSearchedArray] = useState([]);
 
   const getProjects = async () => {
-    await fetch(
-      "./data/projects.json",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-      }
-    )
+    await fetch("./data/projects.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
       .then((res) => res.json())
       .then((data) => {
         setProjects(data);
+        setSearchedArray(data);
       });
   };
 
@@ -29,12 +29,42 @@ export default function Projects() {
     getProjects();
   }, []);
 
+  useEffect(() => {
+    setProjects(
+      searchedArray.filter(
+        (projName) =>
+          projName.title.toLowerCase().includes(searchText.toLowerCase()) ||
+          projName.desc.toLowerCase().includes(searchText.toLowerCase()) ||
+          projName.status.toLowerCase().includes(searchText.toLowerCase()) ||
+          projName.date.toLowerCase().includes(searchText.toLowerCase())
+
+
+      )
+    );
+  }, [searchText]);
+
   return (
     <div className="flow-container">
-       <p className="my-proj"><Link to="/" className="back-to-home"><i className="fa fa-chevron-left" aria-hidden="true"></i></Link> Latest Projects</p>
-       {projects.slice(0).reverse().map((project, index) => (
-        <Project project={project} key={index} />
-       ))}
+      <p className="my-proj">
+        <Link to="/" className="back-to-home">
+          <i className="fa fa-chevron-left" aria-hidden="true"></i>
+        </Link>{" "}
+        Latest Projects
+      </p>
+      <div className="search-box">
+        <input
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+          placeholder="Search projects..."
+        />
+      </div>
+      {projects
+        .slice(0)
+        .reverse()
+        .map((project, index) => (
+          <Project project={project} key={index} />
+        ))}
 
       <p className="more-proj-text">More projects on my Linkedin and Github</p>
       <p className="copyr">Copyright © {date} Thirasha Praween</p>
